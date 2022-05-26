b6c4fa6c2fdd082bfb67189fee53854d3098da81
Celanoa <nathaniel.alteza@gmail.com>
2022-05-26 12:37:08 +0800

    update for 128x128 input

---
+++ datasets/humans_vs_robots_simplenet-v2.py
@@ -0,0 +1,79 @@
+###################################################################################################
+#
+# Copyright (C) 2018-2020 Maxim Integrated Products, Inc. All Rights Reserved.
+#
+# Maxim Integrated Products, Inc. Default Copyright Notice:
+# https://www.maximintegrated.com/en/aboutus/legal/copyrights.html
+#
+###################################################################################################
+#
+# Portions Copyright (c) 2018 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+"""
+Face Dataset
+"""
+import os
+
+import torchvision
+from torchvision import datasets, transforms
+
+import ai8x
+
+
+
+def get_datasets(data, load_train=True, load_test=True):
+    (data_dir, args) = data
+
+    if load_train:
+        train_transform = transforms.Compose([
+            transforms.Resize((128,128)), #
+            transforms.RandomCrop((128,128), padding=4), #uncomment to data augment
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomRotation(degrees=10),
+            transforms.ColorJitter(brightness=0.3),
+            transforms.GaussianBlur((3,3),(0.1,1)), 
+            transforms.ToTensor(),
+            ai8x.normalize(args=args)
+        ])
+        train_dataset = torchvision.datasets.ImageFolder(os.path.join(data_dir, 'FaceSpoof', 'train'), transform=train_transform)
+
+                                
+    else:
+        train_dataset = None
+
+    if load_test:
+        test_transform = transforms.Compose([
+            transforms.Resize((128,128)), #32
+            transforms.ToTensor(),
+            ai8x.normalize(args=args)
+        ])
+        test_dataset = torchvision.datasets.ImageFolder(os.path.join(data_dir, 'FaceSpoof', 'test'), transform=test_transform)
+
+        if args.truncate_testset:
+            test_dataset.data = test_dataset.data[:1]
+    else:
+        test_dataset = None
+
+    return train_dataset, test_dataset
+
+
+datasets = [
+    {
+        'name': 'humans_vs_robotsv2',
+        'input': (3, 128, 128),
+        'output': ('client', 'imposter'),
+        'loader': get_datasets,
+    },
+]
