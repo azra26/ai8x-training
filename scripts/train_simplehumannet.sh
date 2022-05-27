084051b2b9bb312ee0acc60ed39a1ac192274162
Celanoa <nathaniel.alteza@gmail.com>
2022-05-27 08:46:39 +0800

    Update qat_policy to match simplehumannetv2

--- scripts/train_simplehumannet.sh
+++ scripts/train_simplehumannet.sh
@@ -3,5 +3,5 @@
 
 #python train.py --epochs 30 --optimizer Adam --lr 0.0001 --weight-decay 0.05 --compress policies/schedule-simplehumannet.yaml --enable-tensorboard --pr-curves --model ai85simplehumannet --dataset humans_vs_robots_simplenet --device MAX78000 --batch-size 128 --print-freq 100 --confusion --qat-policy policies/qat_policy_simplehumannet.yaml --param-hist --use-bias "$@"
 
-python train.py --epochs 30 --optimizer Adam --lr 0.0001 --weight-decay 0.05 --compress policies/schedule-simplehumannet.yaml --enable-tensorboard --pr-curves --model ai85simplehumannetv2 --dataset humans_vs_robotsv2 --device MAX78000 --batch-size 128 --print-freq 100 --confusion --qat-policy policies/qat_policy_simplehumannet.yaml --param-hist --use-bias "$@"
+python train.py --epochs 30 --optimizer Adam --lr 0.0001 --weight-decay 0.05 --compress policies/schedule-simplehumannet.yaml --enable-tensorboard --pr-curves --model ai85simplehumannetv2 --dataset humans_vs_robotsv2 --device MAX78000 --batch-size 128 --print-freq 100 --confusion --qat-policy policies/qat_policy_simplehumannetv2.yaml --param-hist --use-bias "$@"
 
