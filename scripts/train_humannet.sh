152b20fe59801fc3f94fe94d44c4f2a845da4bd0
Jay Patrick Nieles <jaypnieles@gmail.com>
2022-05-30 18:44:05 +0800

    Added weight decay and deterministic in train
    Signed-off-by: Jay Patrick Nieles <jaypnieles@gmail.com>

--- scripts/train_humannet.sh
+++ scripts/train_humannet.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python train.py --epochs 100 --optimizer Adam --lr 0.001 --compress policies/schedule-humannet.yaml --model ai85humannet --dataset humans_vs_robots --batch-size 100 --device MAX78000 --enable-tensorboard --pr-curves --confusion --param-hist --embedding "$@"
+python train.py --epochs 100 --optimizer Adam --lr 0.001 --wd 0 --deterministic --compress policies/schedule-humannet.yaml --model ai85humannet --dataset humans_vs_robots --batch-size 100 --device MAX78000 --enable-tensorboard --pr-curves --confusion --param-hist --embedding "$@"
